6950bde46b3964796a76cc5e6aebcf0c8b4b9087
AokumoRin <parksehyun2025@gmail.com>
2025-10-15 19:31:17 +0900

    Create

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log(`Server is running http://0.0.0.0:${process.env.PORT}`);
+});
