a157454c444e2d5704a9fc9639b88ee90869cf30
AokumoRin <parksehyun2025@gmail.com>
2025-10-15 19:38:25 +0900

    Re

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -13,6 +15,6 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.listen(3000, "0.0.0.0", () => {
-    console.log(`Server is running http://0.0.0.0:${process.env.PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running http://0.0.0.0:${PORT}`);
 });
